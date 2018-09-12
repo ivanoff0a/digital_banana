8e357c16b19d801bc3e02c19ad3fd516abc3d0ec
kormyshov <kormyshov@yandex-team.ru>
2018-09-12 21:31:24 +0300

    Add read_all_file

---
+++ common/read_all_file.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    int a;
+    while (cin >> a) {
+        cout << a << " ";
+    }
+    return 0;
+}
---
+++ common/read_all_file.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+g++ -o read_all_file read_all_file.cpp
+./read_all_file
